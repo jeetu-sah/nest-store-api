f29f81386acb75a8d358570aa912d44f978a8e89
Jitendra <jitendrasahu17996@gmail.com>
2024-07-22 18:54:55 +0530

    hello world changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome in this application!';
+    return 'welcome in this !';
   }
 }
