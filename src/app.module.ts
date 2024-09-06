7eb830bea9b2cf3fd4bbd42501b558bedc79bc0b
Aryansahu <aryansahuosvn2006@gmail.com>
2024-09-07 00:23:15 +0530

    chnages in database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
+
 export class AppModule {}
