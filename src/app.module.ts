90f35ccbeb5ce71cc266f1fdf4f2b2042a04ef76
Jitendra <jitendrasahu17996@gmail.com>
2024-09-07 01:05:45 +0530

    connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -14,9 +15,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      password: '',
+      database: 'nest_database',
+      entities: [User],
       synchronize: true,
     })
   ],
