ae515c4217fb3fed07a47f5aed9b748e13d8b66e
Jitendra <jitendrasahu17996@gmail.com>
2024-09-23 01:02:45 +0530

    added quesry builder

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Post, Req, Res, HttpStatus, Delete, Param, Put, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  HttpStatus,
+  Delete,
+  Param,
+  Put,
+  Patch,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   @Get('/')
   find() {
@@ -27,11 +38,11 @@ export class UserController {
       const deleteResponse = await this.userService.delete(id);
       res.status(HttpStatus.OK).json(deleteResponse);
     } else {
-      res.status(HttpStatus.OK).json({ data: null, msg: 'User Does not exists' });
-
+      res
+        .status(HttpStatus.OK)
+        .json({ data: null, msg: 'User Does not exists' });
     }
   }
-
 
   @Get('/:id')
   async details(@Param('id') id: number, @Res() res: Response) {
@@ -40,12 +51,15 @@ export class UserController {
       res.status(HttpStatus.OK).json(detailsResponse);
     } else {
       res.status(HttpStatus.OK).json({ data: null, msg: 'no record found' });
-
     }
   }
 
   @Patch('/:id')
-  async updateUser(@Param('id') id: number, @Req() request: Request, @Res() res: Response) {
+  async updateUser(
+    @Param('id') id: number,
+    @Req() request: Request,
+    @Res() res: Response,
+  ) {
     // return 'test';
     const user = await this.userService.details(id);
     if (user) {
@@ -65,19 +79,17 @@ export class UserController {
     const mobile = request.body.mobile;
     //request.body -> for all form data
     //request.body.first_name -> for single key data
-    const userEmail = await this.userService.findUserByFieldName({ 'email': email });
-    const userMobile = await this.userService.findUserByFieldName({ 'mobile': mobile });
-
-
-    if (userEmail) {
-      res.status(HttpStatus.OK).json({ "msg": 'this mail is already exist please enter new mail..' })
+    const user = await this.userService.checkUserMobileEmail(email, mobile);
+    if (user.length == 0) {
+      const userResult = await this.userService.create(request.body);
+      return res
+        .status(HttpStatus.OK)
+        .json({ msg: 'User created successfully', data: userResult });
+    } else {
+      return res.status(HttpStatus.OK).json({
+        msg: 'This mobile and email is already exists',
+        data: null,
+      });
     }
-    else if (userMobile) {
-      res.status(HttpStatus.OK).json({ "msg": 'this number is already exist please enter new number..' })
-    }
-
-    const userResult = await this.userService.create(request.body);
-    res.status(HttpStatus.OK).json(userResult);
   }
-
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -24,29 +24,40 @@ export class UserService {
   }
 
   async delete(id: number) {
-    // return "test in services" 
+    // return "test in services"
     return this.userRepository.delete(id);
   }
 
   async details(id: number) {
-    // return "test in services" 
+    // return "test in services"
     return this.userRepository.findOneBy({ id });
   }
 
   async findUserByFieldName(fieldObject: object) {
-    // return "test in services" 
+    // return "test in services"
     return this.userRepository.findOneBy(fieldObject);
   }
 
   async update(user) {
-    // return "test in services" 
+    // return "test in services"
     return this.userRepository.save(user);
-  } 
-
+  }
 
   async registration(user: Partial<User>): Promise<User> {
     const newuser = this.userRepository.create(user);
     return this.userRepository.save(newuser);
   }
 
+  async checkUserMobileEmail(email: string, mobile: number): Promise<User[]> {
+    return this.userRepository.find({
+      where: [
+        {
+          email: email,
+        },
+        {
+          mobile: mobile,
+        },
+      ],
+    });
+  }
 }
